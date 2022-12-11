6a694deb314ff01b902127ddea237e86a9fa8730
Medrenguard <0892364395@mail.ru>
2022-12-11 17:33:46 +0300

    убирает много лишних элементов, добавляет первый svg-регион

---
+++ src/views/mainView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <regionUnderFog />
+  </div>
+</template>
+
+<script>
+import regionUnderFog from '@/components/regions/regionUnderFog.vue'
+// margin: 0px 0px -32.8px -37.8px
+export default {
+  name: 'mainView',
+  components: {
+    regionUnderFog
+  }
+}
+</script>
