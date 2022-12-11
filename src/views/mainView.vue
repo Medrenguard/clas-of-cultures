aa676311ab03f30e9f81cdb6b4b4972dca76c1d2
Medrenguard <0892364395@mail.ru>
2022-12-11 20:09:33 +0300

    Добавляет раскладку на 4-ых

--- src/views/mainView.vue
+++ src/views/mainView.vue
@@ -29,7 +29,7 @@ export default {
     }
   },
   created () {
-    this.currentCountGamers = 2
+    this.currentCountGamers = 4
   },
   mounted () {
   },
@@ -38,8 +38,8 @@ export default {
       // размер тайла на данный момент - 32.7 пикселя
       const gamers = this.currentCountGamers
       const tileHeight = 32.7
-      const sideByX = 32.7 * 1.724770642201835 // сдвиг по оси X для раскадки на двоих
       if (gamers === 2) {
+        const sideByX = 32.7 * 1.725770642201835 // сдвиг по оси X для раскадки на двоих
         switch (regionNum) {
           case 2:
             return `${tileHeight}px 0px 0px -${sideByX}px`
@@ -59,6 +59,45 @@ export default {
             return `${tileHeight * 5}px 0px 0px ${sideByX}px`
           case 10:
             return `${tileHeight * 6}px 0px 0px 0px`
+        }
+      } else if (gamers === 4) {
+        const baseSideByX = 32.7 * 1.725770642201835 // сдвиг по оси X для раскадки на четверых для большинства тайлов
+        const startSideByX = 32.7 * 2.596330275229358 // сдвиг по оси X для раскадки на четверых для стартовых тайлов
+        switch (regionNum) {
+          case 2:
+            return `-${tileHeight * 0.5}px 0px 0px -${startSideByX}px`
+          case 3:
+            return `-${tileHeight * 0.5}px 0px 0px ${startSideByX}px`
+          case 4:
+            return `${tileHeight}px 0px 0px -${baseSideByX}px`
+          case 5:
+            return `${tileHeight}px 0px 0px ${baseSideByX}px`
+          case 6:
+            return `${tileHeight * 2}px 0px 0px -${baseSideByX * 2}px`
+          case 7:
+            return `${tileHeight * 2}px 0px 0px 0px`
+          case 8:
+            return `${tileHeight * 2}px 0px 0px ${baseSideByX * 2}px`
+          case 9:
+            return `${tileHeight * 3}px 0px 0px -${baseSideByX}px`
+          case 10:
+            return `${tileHeight * 3}px 0px 0px ${baseSideByX}px`
+          case 11:
+            return `${tileHeight * 4}px 0px 0px -${baseSideByX * 2}px`
+          case 12:
+            return `${tileHeight * 4}px 0px 0px 0px`
+          case 13:
+            return `${tileHeight * 4}px 0px 0px ${baseSideByX * 2}px`
+          case 14:
+            return `${tileHeight * 5}px 0px 0px -${baseSideByX}px`
+          case 15:
+            return `${tileHeight * 5}px 0px 0px ${baseSideByX}px`
+          case 16:
+            return `${tileHeight * 6}px 0px 0px 0px`
+          case 17:
+            return `${tileHeight * 6.5}px 0px 0px -${startSideByX}px`
+          case 18:
+            return `${tileHeight * 6.5}px 0px 0px ${startSideByX}px`
         }
       }
     }
