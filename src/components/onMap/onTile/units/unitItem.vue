d61e94d8b5d77ba274ec80b1dc3bfe91e0a6ad7b
Medrenguard <0892364395@mail.ru>
2023-05-14 19:12:30 +0300

    Корректирует остальные вызовы к обновлённным юнитам в сторе, вводит компонент unitItem

---
+++ src/components/onMap/onTile/units/unitItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <g
+    inkscape:groupmode="layer"
+    class="unit-item"
+    :class="colorClass"
+    inkscape:label="Юнит">
+      <settler-item v-if="unitType === 'settler'" :settlerID="unitID" :settlerOwner="unitOwner"/>
+      <infantry-item v-else-if="unitType === 'infantry'" :infantryID="unitID" :infantryOwner="unitOwner"/>
+      <ship-item v-else-if="unitType === 'ship'" :shipID="unitID" :shipOwner="unitOwner"/>
+  </g>
+</template>
+
+<script>
+import settlerItem from '@/components/onMap/onTile/units/settlerItem.vue'
+import infantryItem from '@/components/onMap/onTile/units/infantryItem.vue'
+import shipItem from '@/components/onMap/onTile/units/shipItem.vue'
+
+export default {
+  name: 'unitItem',
+  components: {
+    settlerItem,
+    infantryItem,
+    shipItem
+  },
+  props: {
+    unitID: Number,
+    unitType: String,
+    unitOwner: String,
+    colorClass: String
+  },
+  mounted () {
+  },
+  computed: {
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
